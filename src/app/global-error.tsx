904a6362d527a2f70e14c718e1c6f06dc8fc7c32
RaeII <israel.zeferino@hotmail.com>
2026-02-06 10:36:28 -0300

    feat: Not found

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,24 +1,23 @@
-"use client";
+// Error boundaries must be Client Components
+'use client';
+// FORCING DYNAMIC HERE ↓
+export const dynamic = 'force-dynamic';
 
-import { useEffect } from "react";
 
-export default function GlobalError({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
+const GlobalError = ({ error, reset }: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) => {
 
   return (
+    // global-error must include html and body tags
     <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
-      </body>
+    <body>
+    <h2>Something went wrong!</h2>
+    <button onClick={() => reset()}>Try again</button>
+    </body>
     </html>
   );
 }
+
+export default GlobalError;
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
+  DropdownMenu, 
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
